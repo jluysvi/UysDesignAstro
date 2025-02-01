d56da78af0940ae8ff2d473ecf2a092d300897d2
Cobus Uys <jluysvi@gmail.com>
2025-02-01 11:44:35 +0200

    Starting to build out the real site now.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,5 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [require("@tailwindcss/typography"), require('daisyui'),],
-	daisyui: {
-		themes: ["light", "dark", "corporate",],
-	},
+	plugins: [require("@tailwindcss/typography"), ],
 }
