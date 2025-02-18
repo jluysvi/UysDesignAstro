a11245fb44493e20965065b9f304ec012cb3693a
Cobus Uys <jluysvi@gmail.com>
2025-02-18 16:05:27 +0200

    Remove pricing section, changed some words.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://uysdesign.co.za/',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  typescript: {
+    checkCondfig: false,
+    check: false
+  }
 });
